645941cfb556064687db666a5c8b0f70662ce520
Abdul Haseeb <abdulh717@gmail.com>
2025-09-06 17:49:08 +0500

    taking input from user

---
+++ 4-User-Input.py
@@ -0,0 +1,14 @@
+# Simple Input Statement:
+
+# input('Name: ')
+
+# Store Input in a variable
+
+# name = input('Name: ')
+# print(name)
+
+# Multiple Inputs
+
+name = input('Name: ')
+age = input('Age: ')
+print('Hello', name, ', you are', age, 'years old.')
