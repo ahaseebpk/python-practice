6a318ef92fca3f4f9b7a34f2fe140931abbdcea0
Abdul Haseeb <abdulh717@gmail.com>
2025-09-06 17:44:11 +0500

    variables declarations

---
+++ 3-Variables.py
@@ -0,0 +1,24 @@
+# Simple variable assignment and printing
+hello = 'tim'
+print(hello)
+
+# printing multiple variables in a single statement
+hello = 'tim'
+world = 'world'
+print(hello, world)
+
+# Naming Conventions
+
+# No special characters (other than than underscore)
+# snake case (hello_world) (prefer this for python)
+# camel case (helloWorld)
+# pascal case (HelloWorld)
+
+# No numbers at the beginning
+# no spaces
+# no keywords
+# no built-in functions
+# no built-in modules
+# no built-in classes
+
+# e.g. 9hello = 'tim' will raise an error
