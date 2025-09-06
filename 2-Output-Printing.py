436795cb1bc0f7577ddaaa6e9fecf4e336833811
Abdul Haseeb <abdulh717@gmail.com>
2025-09-06 17:37:10 +0500

    something more on printing

--- 2-Output-Printing.py
+++ 2-Output-Printing.py
@@ -1,1 +1,7 @@
+# Print string
 print("Hello World!")
+
+# Print many things in the same line
+# \ escapes character, \n means carriage return (enter or next line)
+print("Hello", 'end', 87, False, end='\n')
+print("hello")
