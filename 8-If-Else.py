288c419af729ade8d01b1522fb1862fb5361aab5
Abdul Haseeb <abdulh717@gmail.com>
2025-09-07 00:51:05 +0500

    if else blocks

---
+++ 8-If-Else.py
@@ -0,0 +1,10 @@
+x = input('Name: ')
+if x == 'Abdul':
+    print('You are great!')
+    print() # => Prints empty space
+elif x == 'Joe':
+    print('Bye Joe!')
+else:
+    print('No')
+
+print('Always do this!')
