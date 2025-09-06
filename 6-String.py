eac56018bb9080b2b0504a3d366581ccde85f641
Abdul Haseeb <abdulh717@gmail.com>
2025-09-06 22:59:51 +0500

    some more on strings

--- 6-String.py
+++ 6-String.py
@@ -11,3 +11,14 @@ print(hello.count("LL")) # => 1, hello: "HELLO"
 
 # Method chaining:
 print('HELLO WORLD'.lower().count('o'))
+
+# Multiply Operator:
+x = "hello"
+y = 3
+# Even these are different data types, but for strings, * operator can be used to multiply them
+print(x * 3) # => hellohellohello
+
+# + Operator: Used to concatenate two strings 
+x = "hello"
+y = "world"
+print(x+y)
